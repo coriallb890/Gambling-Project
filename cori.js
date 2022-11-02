2525abfbcb314ffc2ef92e3c71a99a7a401b33f2
coriallb890 <62068593+coriallb890@users.noreply.github.com>
2022-11-01 19:36:18 -0500

    Game is made, time for aesthetics

--- cori.js
+++ cori.js
@@ -1,7 +1,19 @@
-let coins = 0;
+let coins = 0
+let d1 = 0
+let d2 = 0
+let d3 = 0
+let sum = 0
+let lastSum = 0
+let play = false
 
-const addBtn = document.getElementById("add-coins")
-const currCoins = document.getElementById("coins")
+const currCoins = document.getElementById("coins-count")
+const playbtn = document.getElementById("play-btn")
+const lowbtn = document.getElementById("lower-btn")
+const highbtn = document.getElementById("higher-btn")
+const sd1 = document.getElementById("d1")
+const sd2 = document.getElementById("d2")
+const sd3 = document.getElementById("d3")
+const total = document.getElementById("sum")
 
 let fromLocal = JSON.parse(localStorage.getItem("coins"))
 
@@ -14,8 +26,74 @@ function render(count){
     currCoins.innerHTML = count
 }
 
-addBtn.addEventListener("click", function(){
-    coins += 1
-    localStorage.setItem("coins", JSON.stringify(coins))
-    render(coins)
+function diceRoll(){
+    return Math.floor(Math.random() * 6) + 1
+}
+
+function roll(){
+    d1 = diceRoll()
+    d2 = diceRoll()
+    d3 = diceRoll()
+    sd1.innerHTML = d1
+    sd2.innerHTML = d2
+    sd3.innerHTML = d3
+    sum = d1 + d2 + d3
+    total.innerHTML = sum
+}
+
+playbtn.addEventListener("click", function(){
+    if(coins >= 10){
+        play = true
+        coins -= 10
+        localStorage.setItem("coins", JSON.stringify(coins))
+        render(coins)
+        roll()
+    }
+    else{
+        alert("Not enough coins! Go back to home for more")
+    }
 })
+
+lowbtn.addEventListener("click", function(){
+    if(play){
+        lastSum = sum
+        roll()
+        if(sum > lastSum){
+            alert("Wrong! " + sum + " is higher than " + lastSum)
+            play = false
+        }
+        else{
+            coins += 3
+            localStorage.setItem("coins", JSON.stringify(coins))
+            render(coins)
+        }
+    }
+    else{
+        alert("Press play first!")
+    }
+})
+
+highbtn.addEventListener("click", function(){
+    if(play){
+        lastSum = sum
+        roll()
+        if(sum < lastSum){
+            render(coins)
+            alert("Wrong! " + sum + " is lower than " + lastSum)
+            play = false
+        }
+        else{
+            coins += 3
+            localStorage.setItem("coins", JSON.stringify(coins))
+            render(coins)
+        }
+    }
+    else{
+        alert("Press play first!")
+    }
+})
+
+
+
+
+
