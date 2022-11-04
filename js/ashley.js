30dda3a74ee499723bdb185a7c976b4b42cb04d6
Ashley Palmer <ashley-palmer@suddenlink.net>
2022-11-04 14:55:27 -0500

    added some colors

--- js/ashley.js
+++ js/ashley.js
@@ -1,3 +1,13 @@
+// CARD MATCHING FUNCTIONALITY //
+
+const yelloworange = "#ffad08"
+const yellow = "#edd75a"
+const green = "#73b06f"
+const teal = "#0c8f8f"
+const slategray = "#405059"
+
+const colors = [yelloworange, yellow, green, teal, slategray]
+
 let coins = 0;
 
 const addBtn = document.getElementById("add-coins")
