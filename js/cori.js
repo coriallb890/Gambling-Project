fa2e4ca1bc1f453f1a0ef67a16883a7100531d4f
coriallb890 <62068593+coriallb890@users.noreply.github.com>
2022-11-04 16:29:35 -0500

    reorganized and me game mostly done

--- js/cori.js
+++ js/cori.js
@@ -14,6 +14,8 @@ const sd1 = document.getElementById("d1")
 const sd2 = document.getElementById("d2")
 const sd3 = document.getElementById("d3")
 const total = document.getElementById("sum")
+const cup = document.getElementById("cup")
+const dice = document.getElementById("dice")
 
 let fromLocal = JSON.parse(localStorage.getItem("coins"))
 
@@ -25,25 +27,32 @@ if(fromLocal){
 function loadDice(diceNum){
     switch(diceNum){
         case 1:
-            return '<img src="./images/1.png"></img>'
+            return '<img src="../images/1.png"></img>'
         case 2:
-            return '<img src="./images/2.png"></img>'
+            return '<img src="../images/2.png"></img>'
         case 3:
-            return '<img src="./images/3.png"></img>'
+            return '<img src="../images/3.png"></img>'
         case 4:
-            return '<img src="./images/4.png"></img>'
+            return '<img src="../images/4.png"></img>'
         case 5:
-            return '<img src="./images/5.png"></img>'
+            return '<img src="../images/5.png"></img>'
         case 6:
-			return '<img src="./images/6.png"></img>'
+			return '<img src="../images/6.png"></img>'
     }
 
 }
 
+function resetAnimation(){
+    cup.style.animation = "none"
+    cup.style.display = "inline"
+    sd1.innerHTML = ""
+    sd2.innerHTML = ""
+    sd3.innerHTML = ""
+}
+
 function resetGame(){
-    sd1.innerHTML = "Dice 1"
-    sd2.innerHTML = "Dice 2"
-    sd3.innerHTML = "Dice 3"
+    playbtn.style.backgroundColor = "limegreen"
+    resetAnimation()
     total.innerHTML = 0
 }
 
@@ -56,46 +65,45 @@ function diceRoll(){
 }
 
 function roll(){
+    cup.style.display = "none"
     d1 = diceRoll()
     d2 = diceRoll()
     d3 = diceRoll()
+    //dice.style.display = "grid"
     sd1.innerHTML = loadDice(d1)
     sd2.innerHTML = loadDice(d2)
     sd3.innerHTML = loadDice(d3)
     sum = d1 + d2 + d3
+    console.log("in roll")
+    console.log(sum)
     total.innerHTML = sum
 }
 
 playbtn.addEventListener("click", function(){
-    if(coins <= 10){
+    if(coins < 10){
         alert("Not enough coins! Go back to home for more")
     }
     else if (play == true) {
         alert("Your already playing, go ahead and take your guess!")
     } 
     else{
+        playbtn.style.backgroundColor = "gray"
         play = true
         coins -= 10
         localStorage.setItem("coins", JSON.stringify(coins))
         render(coins)
-        roll()
+        cup.style.animation = "test 1s linear normal"
+        setTimeout(roll, 1000)
     }
 })
 
+
 lowbtn.addEventListener("click", function(){
     if(play){
+        resetAnimation()
         lastSum = sum
-        roll()
-        if(sum > lastSum){
-            alert("Wrong! " + sum + " is higher than " + lastSum)
-            play = false
-            resetGame()
-        }
-        else{
-            coins += 3
-            localStorage.setItem("coins", JSON.stringify(coins))
-            render(coins)
-        }
+        cup.style.animation = "test 1s linear normal"
+        setTimeout(checkLow, 1000)
     }
     else{
         alert("Press play first!")
@@ -104,26 +112,44 @@ lowbtn.addEventListener("click", function(){
 
 highbtn.addEventListener("click", function(){
     if(play){
+        resetAnimation()
         lastSum = sum
-        roll()
-        if(sum < lastSum){
-            render(coins)
-            alert("Wrong! " + sum + " is lower than " + lastSum)
-            play = false
-            resetGame()
-        }
-        else{
-            coins += 3
-            localStorage.setItem("coins", JSON.stringify(coins))
-            render(coins)
-        }
+        cup.style.animation = "test 1s linear normal"
+        setTimeout(checkHigh, 1000)
+        
     }
     else{
         alert("Press play first!")
     }
 })
 
+function addCoins(){
+    coins += 2
+    localStorage.setItem("coins", JSON.stringify(coins))
+    render(coins)
+}
 
-
+function checkHigh(){
+    roll()
+    if(sum < lastSum){
+        alert("Wrong! " + sum + " is lower than " + lastSum)
+        play = false
+        resetGame()
+    }
+    else{
+        addCoins()
+    }
+}
+function checkLow(){
+    roll()
+    if(sum > lastSum){
+        alert("Wrong! " + sum + " is higher than " + lastSum)
+        play = false
+        resetGame()
+    }
+    else{
+        addCoins()
+    }
+}
 
 
