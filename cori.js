00fb76657f32933674d1fbe7d287cbdfa748101f
coriallb890 <therealcori1@gmail.com>
2022-11-04 07:11:53 -0500

    some of my stuff done

--- cori.js
+++ cori.js
@@ -22,6 +22,31 @@ if(fromLocal){
     render(coins)
 }
 
+function loadDice(diceNum){
+    switch(diceNum){
+        case 1:
+            return '<img src="./images/1.png"></img>'
+        case 2:
+            return '<img src="./images/2.png"></img>'
+        case 3:
+            return '<img src="./images/3.png"></img>'
+        case 4:
+            return '<img src="./images/4.png"></img>'
+        case 5:
+            return '<img src="./images/5.png"></img>'
+        case 6:
+			return '<img src="./images/6.png"></img>'
+    }
+
+}
+
+function resetGame(){
+    sd1.innerHTML = "Dice 1"
+    sd2.innerHTML = "Dice 2"
+    sd3.innerHTML = "Dice 3"
+    total.innerHTML = 0
+}
+
 function render(count){
     currCoins.innerHTML = count
 }
@@ -34,23 +59,26 @@ function roll(){
     d1 = diceRoll()
     d2 = diceRoll()
     d3 = diceRoll()
-    sd1.innerHTML = d1
-    sd2.innerHTML = d2
-    sd3.innerHTML = d3
+    sd1.innerHTML = loadDice(d1)
+    sd2.innerHTML = loadDice(d2)
+    sd3.innerHTML = loadDice(d3)
     sum = d1 + d2 + d3
     total.innerHTML = sum
 }
 
 playbtn.addEventListener("click", function(){
-    if(coins >= 10){
+    if(coins <= 10){
+        alert("Not enough coins! Go back to home for more")
+    }
+    else if (play == true) {
+        alert("Your already playing, go ahead and take your guess!")
+    } 
+    else{
         play = true
         coins -= 10
         localStorage.setItem("coins", JSON.stringify(coins))
         render(coins)
         roll()
-    }
-    else{
-        alert("Not enough coins! Go back to home for more")
     }
 })
 
@@ -61,6 +89,7 @@ lowbtn.addEventListener("click", function(){
         if(sum > lastSum){
             alert("Wrong! " + sum + " is higher than " + lastSum)
             play = false
+            resetGame()
         }
         else{
             coins += 3
@@ -81,6 +110,7 @@ highbtn.addEventListener("click", function(){
             render(coins)
             alert("Wrong! " + sum + " is lower than " + lastSum)
             play = false
+            resetGame()
         }
         else{
             coins += 3
